128ada166475d44ee7fc41bddd874d182f00de0b
eropka <yeger2021@mail.ru>
2017-05-25 15:04:47 +0300

    node+gulp working version

---
+++ server.js
@@ -0,0 +1,32 @@
+class Server {
+  bootstrap(env) {
+    this.express = require('express');
+    this.app = this.express();
+    this.clientRouter = this.express.Router();
+
+    this.app.disable('x-powered-by');
+    this.app.set('view engine', 'jade');
+    this.app.set('view cache', this.env === 'production');
+
+    this.setupRouting();
+    this.app.listen(8880);
+  }
+
+  setupRouting() {
+    const path = require('path');
+    console.log(path);
+    this.app.use('/build', this.express.static(path.join(__dirname, 'build'), {
+      maxAge: this.env === 'production' ? 31536000 : 0
+    }));
+
+    this.clientRouter.get('/', (req, res, next) => {
+      res.render('index', {
+        pageTitle: 'Main page',
+      })
+    });
+
+    this.app.use('/', this.clientRouter);
+  }
+}
+
+Object.create(Server.prototype).bootstrap(process.env.NODE_ENV || 'development');
