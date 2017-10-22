ce477350d6526c4c42f0106a672a1929c8d53b16
eropka <yeger2021@mail.ru>
2017-10-22 13:39:45 +0300

    add holes in triangle shape

--- assets/js/directives/kitEditor.js
+++ assets/js/directives/kitEditor.js
@@ -192,15 +192,15 @@ module.exports = ['d3Factory',
               .attr('data-kit-custom-shape', '')  
               .attr('data-kit-t-shape', '').node()))($scope);  //node() - d3 -> native
 
-            // $compile(angular.element($scope.editor.svg.container.append('g')  //angular.element -native -> angular
-            //   .attr('transform', 'translate(100, 100)')
-            //   .attr('data-kit-custom-shape', '')  
-            //   .attr('data-kit-gear', '').node()))($scope);  //node() - d3 -> native
+            $compile(angular.element($scope.editor.svg.container.append('g')  //angular.element -native -> angular
+              .attr('transform', 'translate(100, 100)')
+              .attr('data-kit-custom-shape', '')  
+              .attr('data-kit-gear', '').node()))($scope);  //node() - d3 -> native
 
-            // $compile(angular.element($scope.editor.svg.container.append('g')  //angular.element -native -> angular
-            //   .attr('transform', 'translate(100, 100)')
-            //   .attr('data-kit-custom-shape', '')  
-            //   .attr('data-kit-screw', '').node()))($scope);  //node() - d3 -> native
+            $compile(angular.element($scope.editor.svg.container.append('g')  //angular.element -native -> angular
+              .attr('transform', 'translate(100, 100)')
+              .attr('data-kit-custom-shape', '')  
+              .attr('data-kit-screw', '').node()))($scope);  //node() - d3 -> native
 
             $compile(angular.element($scope.editor.svg.container.append('g')  //angular.element -native -> angular
               .attr('transform', 'translate(550, 450)')
--- assets/js/directives/kitTriangle.js
+++ assets/js/directives/kitTriangle.js
@@ -8,52 +8,52 @@ module.exports = ['d3Factory', function(d3Factory) {
         $scope.shape.moniker = 'core.screw';
         function drawTriangle(d3, holder, pixelsPerMm,
           holeRadius, hHoleCount, vHoleCount) {
-
+            
             function drawTriangleWithHoles(holeRadius, hHoleCount, vHoleCount) {
               var borderRadius = 2 * holeRadius;
-              var width = borderRadius * hHoleCount;
-              var height = borderRadius * vHoleCount;
-              var vDirection = Math.abs(height)/height;
-              var hDirection = Math.abs(width)/width;
-              var angle = Math.atan(width/height);
-              var rotation = (vDirection == hDirection) ? 0 : 1;              
+              var width = 2 * borderRadius * hHoleCount;
+              var height = 2 * borderRadius * vHoleCount;
+              var angle = Math.atan(width/height);              
               var pathString = [];
+              // console.log(typeof(pathString));
+              
+              var pathString = ['M ', borderRadius *(1 + Math.cos(angle)), ', ',
+                  borderRadius * (Math.sin(angle))];
 
-              var pathString = ['M ', vDirection * borderRadius *(1 + Math.cos(angle)), ', ',
-                  -hDirection * borderRadius * (Math.sin(angle))];
-
-              console.log(vDirection, hDirection, angle, rotation, borderRadius, width, height, pixelsPerMm);
+              // console.log(angle, borderRadius, width, height, pixelsPerMm);
 
               pathString.push(
 
                 //a xradius, yradius, x-axis-rotation large-arc-flag sweep-flag x y
 
-                ' a', borderRadius, ',', borderRadius, ' 0 0,', rotation,
-                hDirection * -(borderRadius *(1 + Math.cos(angle))), ',',          //x coord
-                vDirection * (borderRadius * (Math.sin(angle))),               //y coord
+                ' a', borderRadius, ',', borderRadius, ' 0 0 0,', 
+                 (borderRadius * (1 + Math.cos(angle))), ', ',          //x coord
+                 -(borderRadius * (Math.sin(angle))),               //y coord
 
-                ' v', height + 2 * borderRadius * vDirection,
-                ' a', borderRadius, ',', borderRadius, ' 0 0,', rotation,
-                borderRadius * hDirection, ',', borderRadius * vDirection,
-                ' h', width + 2 * borderRadius * hDirection,
+                ' v', -height + 2 * borderRadius,
+                ' a', borderRadius, ', ', borderRadius, ' 0 0 0,',
+                -borderRadius, ', ', -borderRadius,
+                ' h', -width + 2 * borderRadius,
 
-                ' a', borderRadius, ',', borderRadius, ' 0 0,', rotation,
-                hDirection * (borderRadius * Math.cos(angle)), ',',          //x coord
-                vDirection * -(borderRadius * ( 1 + Math.sin(angle)))               //y coord
-             
+                ' a', borderRadius, ',', borderRadius, ' 0 0 0,',
+                -(borderRadius * Math.cos(angle)), ',',          //x coord
+                (borderRadius * ( 1 + Math.sin(angle))), ' z'               //y coord 
+
               );
+              // console.log(pathString.join(''));
 
-              for (var j = 0; j < vHoleCount; j++) {
-                for (var i = 0; i < hHoleCount; i++) {
-                  pathString += 'M' + (i * borderRadius + borderRadius / 2) + ',' +
-                    (height - borderRadius + j * borderRadius + borderRadius / 2 - holeRadius) +
-                    'a' + holeRadius + ',' + holeRadius + ' 0 0 1 0 ' + ',' + (2 * holeRadius) +
-                    'a' + holeRadius + ',' + holeRadius + ' 0 0 1 0 ' + ',' + -(2 * holeRadius) +
-                    'z';
+              for (var i = 0; i < hHoleCount; i++) {  //vertical
+                for (var j = i; j < vHoleCount; j++) {  //horizontal
+                  pathString.push( 'M' + ( 5 * holeRadius - i * 2 * borderRadius ) + ', ' +
+                    ( -holeRadius - j * 2 * borderRadius) +
+                    ' a' + holeRadius + ', ' + holeRadius + ' 0 0 1 0 ' + ', ' + (2 * holeRadius) +
+                    ' a' + holeRadius + ', ' + holeRadius + ' 0 0 1 0 ' + ', ' + -(2 * holeRadius) +
+                    ' z ');
                 }
               }
-
-              console.log(pathString.join(''));
+              // console.log(pathString);
+              // console.log(typeof(pathString));
+              // console.log(pathString.join(''));
               return pathString.join('');
             }
             
@@ -62,7 +62,7 @@ module.exports = ['d3Factory', function(d3Factory) {
                     .attr('d', drawTriangleWithHoles( holeRadius * pixelsPerMm, hHoleCount, vHoleCount));
           }
           $scope.shape.svg.shapeObject = drawTriangle(d3, $scope.shape.svg.d3Object,
-            $scope.editor.features.pixelsPerMm, 2.5, -4, -4);
+            $scope.editor.features.pixelsPerMm, 2.5, 4, 4);
       });
     }
   }
